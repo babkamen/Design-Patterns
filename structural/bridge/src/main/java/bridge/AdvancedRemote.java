508d6577b0655e0098ec524137813dde4d47b2d3
Roman Lukash <roman.lukash@groupm.tech>
2020-11-23 15:48:39 +0200

    Add bridge pattern

---
+++ structural/bridge/src/main/java/bridge/AdvancedRemote.java
@@ -0,0 +1,17 @@
+package bridge;
+
+public class AdvancedRemote extends Remote {
+
+    public AdvancedRemote(Device device) {
+        super(device);
+    }
+
+    public void toggleMute() {
+        if (device.isMuted()) {
+            device.unmute();
+        } else {
+            device.mute();
+        }
+    }
+
+}
---
+++ structural/bridge/src/test/java/TVTest.java
@@ -0,0 +1,43 @@
+import bridge.AdvancedRemote;
+import bridge.Remote;
+import bridge.TV;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TVTest {
+    @Test
+    public void testRemote() {
+        TV tv = new TV();
+        Remote remote = new Remote(tv);
+
+        remote.togglePower();
+        assertTrue(tv.isEnabled());
+
+        remote.channelDown();
+        assertEquals(0, tv.getChannel());
+
+        remote.channelUp();
+        assertEquals(1, tv.getChannel());
+
+        remote.channelUp();
+        assertEquals(100, tv.getVolume());
+
+        for (int i = 0; i < 100; i++) {
+            remote.volumeDown();
+        }
+        assertEquals(0, tv.getVolume());
+    }
+
+    @Test
+    public void testAdvancedRemote() {
+        TV tv = new TV();
+        AdvancedRemote remote = new AdvancedRemote(tv);
+
+        remote.toggleMute();
+        assertTrue(tv.isMuted());
+
+        remote.toggleMute();
+        assertFalse(tv.isMuted());
+    }
+}
